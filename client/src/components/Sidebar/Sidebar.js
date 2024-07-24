bcc466571205dcb9488ce4781e22112d96ec2ca6
there2989 <there2989@naver.com>
2024-07-24 09:57:16 +0900

    feat: 페이지 전환 시 사이드바 글자 색상 변경 및 요소 정렬

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useLocation } from 'react-router-dom';
 import classes from './Sidebar.module.css';
+import { navigationActions } from '../../store/navigation';
 import logoImage from '../../assets/Layout/Logo_icon.png';
 import loglistIcon from '../../assets/Layout/Loglist_icon.png';
 import mainIcon from '../../assets/Layout/Main_icon.png';
@@ -9,38 +11,62 @@ import monitoringIcon from '../../assets/Layout/Monitoring_icon.png';
 import statisticsIcon from '../../assets/Layout/Statistics_icon.png';
 
 function Sidebar() {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const activePage = useSelector((state) => state.navigation.activePage);
+
+  useEffect(() => {
+    const path = location.pathname.slice(1); // 현재 경로의 슬래시를 제거한 후 설정
+    dispatch(navigationActions.setActivePage(path));
+  }, [location, dispatch]);
+
   return (
     <div className={classes.sidebar}>
       <img src={logoImage} alt="logo_image" className={classes.logoImage} />
       <ul>
         <li>
-          <NavLink to="/main" activeClassName={classes.activeLink}>
+          <NavLink
+            to="/main"
+            className={activePage === 'main' ? classes.activeLink : undefined}
+          >
             <img src={mainIcon} alt="main_icon" className={classes.labelIcon} />
-            메인
+            <span className={activePage === 'main' ? classes.activeText : undefined}>메인</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/loglist" activeClassName={classes.activeLink}>
+          <NavLink
+            to="/loglist"
+            className={activePage === 'loglist' ? classes.activeLink : undefined}
+          >
             <img src={loglistIcon} alt="loglist_icon" className={classes.labelIcon} />
-            조회
+            <span className={activePage === 'loglist' ? classes.activeText : undefined}>조회</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/statistics" activeClassName={classes.activeLink}>
+          <NavLink
+            to="/statistics"
+            className={activePage === 'statistics' ? classes.activeLink : undefined}
+          >
             <img src={statisticsIcon} alt="statistics_icon" className={classes.labelIcon} />
-            통계
+            <span className={activePage === 'statistics' ? classes.activeText : undefined}>통계</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/management" activeClassName={classes.activeLink}>
+          <NavLink
+            to="/management"
+            className={activePage === 'management' ? classes.activeLink : undefined}
+          >
             <img src={managementIcon} alt="management_icon" className={classes.labelIcon} />
-            관리
+            <span className={activePage === 'management' ? classes.activeText : undefined}>관리</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/monitoring" activeClassName={classes.activeLink}>
+          <NavLink
+            to="/monitoring"
+            className={activePage === 'monitoring' ? classes.activeLink : undefined}
+          >
             <img src={monitoringIcon} alt="monitoring_icon" className={classes.labelIcon} />
-            실황
+            <span className={activePage === 'monitoring' ? classes.activeText : undefined}>실황</span>
           </NavLink>
         </li>
       </ul>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-import loginReducer from './login'
+import { configureStore } from '@reduxjs/toolkit';
+import loginReducer from './login';
+import navigationReducer from './navigation';
 
 const store = configureStore({
-    reducer: { login: loginReducer }
-})
+  reducer: {
+    login: loginReducer,
+    navigation: navigationReducer,
+  },
+});
 
-export default store
+export default store;

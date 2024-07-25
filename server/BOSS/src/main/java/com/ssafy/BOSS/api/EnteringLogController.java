7d0f3751c3100923fa2472e44187150320d65313
wonbin <jbs990609@naver.com>
2024-07-25 14:51:04 +0900

    feat: 멤버별 로그 서치 구현

--- server/BOSS/src/main/java/com/ssafy/BOSS/api/EnteringLogController.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/api/EnteringLogController.java
@@ -1,26 +1,46 @@
 package com.ssafy.BOSS.api;
 
 import com.ssafy.BOSS.domain.EnteringLog;
+import com.ssafy.BOSS.domain.Member;
 import com.ssafy.BOSS.dto.enteringLog.EnteringLogSpecifiedDto;
 import com.ssafy.BOSS.dto.enteringLog.UpdateEnteringLog;
+import com.ssafy.BOSS.repository.MemberRepository;
 import com.ssafy.BOSS.service.EnteringLogService;
+import com.ssafy.BOSS.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/loglist")
 public class EnteringLogController {
 
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
     EnteringLogService enteringLogService;
 
     @GetMapping
     public ResponseEntity<?> getEnteringLog(@RequestBody EnteringLogSpecifiedDto dto, @RequestBody Pageable pageable) {
         Page<EnteringLog> logs = enteringLogService.getEnteringLogs(dto, pageable);
         return ResponseEntity.ok(logs);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getEnteringLogByMemberId(@PathVariable long id) {
+        Optional<Member> member = memberRepository.findById(id);
+        if(member.isPresent()) {
+            List<EnteringLog> logs = enteringLogService.findLogsByMember(member);
+            return ResponseEntity.ok(logs);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PutMapping("/{id}")

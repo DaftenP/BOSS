e0369e618734ef30cf5fbec57d351006438a72c4
there2989 <there2989@naver.com>
2024-07-29 11:32:16 +0900

    feat: 메인 페이지, 통계 페이지 간이 구현, 더미데이터 제작 파일 독립화

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,4 +1,5 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 import classes from './Login.module.css';
 import { loginAction } from "../../store/login";
 import sampleImage from '../../assets/Login/Login_background_image.png';
@@ -9,11 +10,26 @@ import loginIcon from '../../assets/Login/Login_icon.png'
 
 function Login() {
   const dispatch = useDispatch();
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const isLogin = useSelector((state) => state.login.isLogin);
 
   const loginHandler = (event) => {
     event.preventDefault();
-    dispatch(loginAction.login());
-  };
+    dispatch(loginAction.login({ id, password }))
+    if (isLogin) {
+      event.preventDefault();
+    } else {
+      alert('로그인에 실패했습니다.')
+    }
+  }
+
+  const handleId = (event) => {
+    setId(event.target.value)
+  }
+  const handlePassword = (event) => {
+    setPassword(event.target.value)
+  }
 
   return (
     <div className={classes.loginContainer} style={{ backgroundImage: `url(${sampleImage})` }}>
@@ -26,18 +42,32 @@ function Login() {
             아이디
             <img src={idIcon} alt="id_icon" className={classes.labelIcon} />
           </label>
-          <input type='text' id='id' placeholder="아이디" className={classes.inputText} />
+          <input 
+            type='text' 
+            id='id' 
+            placeholder="아 이 디" 
+            className={classes.inputText} 
+            value={id} 
+            onChange={handleId} 
+          />
         </div>
         <div className={classes.formGroup}>
           <label htmlFor='password' className={classes.labelText}>
             비밀번호
             <img src={passwordIcon} alt="password_icon" className={classes.labelIcon} />
           </label>
-          <input type='password' id='password' placeholder="비밀번호" className={classes.inputText} />
+          <input 
+            type='password' 
+            id='password' 
+            placeholder="비 밀 번 호" 
+            className={classes.inputText} 
+            value={password} 
+            onChange={handlePassword} 
+          />
         </div>
         <div className={classes.formSubGroup}>
           <div></div>
-          <button className={classes.loginButton}>
+          <button type="submit" className={classes.loginButton}>
             로그인
             <img src={loginIcon} alt="login_icon" className={classes.loginIcon} />
           </button>

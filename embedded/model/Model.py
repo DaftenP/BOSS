e5b8f87d82c209c456f53706a75daffa3e6ece38
daftenp <bjeeyong99@gmail.com>
2024-07-29 17:59:11 +0900

    feat: 실시간 객체 인식 API

--- embedded/model/Model.py
+++ embedded/model/Model.py
@@ -6,4 +6,5 @@ class Model:
     def __init__(self, name: str):
         self.model = YOLO(name)
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print(f'YOLO v8 initialized. device : {self.device}')
         self.model.to(self.device)

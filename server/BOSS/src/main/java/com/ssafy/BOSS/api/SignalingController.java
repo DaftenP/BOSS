26ae80bf7504899fa94f15e8e5221101770a314c
서정운 <sju9417@gmail.com>
2024-07-30 12:43:55 +0900

    feat: SignalingController 로그 추가

--- server/BOSS/src/main/java/com/ssafy/BOSS/api/SignalingController.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/api/SignalingController.java
@@ -1,11 +1,13 @@
 package com.ssafy.BOSS.api;
 
 import com.ssafy.BOSS.domain.SignalMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 public class SignalingController {
     private final SimpMessagingTemplate messagingTemplate;
@@ -17,18 +19,24 @@ public class SignalingController {
     @MessageMapping("/offer")
     @SendTo("/api/topic/offer")
     public SignalMessage offer(SignalMessage message) {
+        log.info("SignalingController::offer");
+        log.info("message: {}", message);
         return message;
     }
 
     @MessageMapping("/answer")
     @SendTo("/api/topic/answer")
     public SignalMessage answer(SignalMessage message) {
+        log.info("SignalingController::answer");
+        log.info("message: {}", message);
         return message;
     }
 
     @MessageMapping("/candidate")
     @SendTo("/api/topic/candidate")
     public SignalMessage candidate(SignalMessage message) {
+        log.info("SignalingController::candidate");
+        log.info("message: {}", message);
         return message;
     }
 }

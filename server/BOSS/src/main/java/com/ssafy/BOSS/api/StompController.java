15997d15e9a18a0e760b345c915312de845bee52
서정운 <sju9417@gmail.com>
2024-07-30 12:44:34 +0900

    feat: StompController 로그 추가

--- server/BOSS/src/main/java/com/ssafy/BOSS/api/StompController.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/api/StompController.java
@@ -1,16 +1,20 @@
 package com.ssafy.BOSS.api;
 
 import com.ssafy.BOSS.domain.EnteringLog;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 public class StompController {
 
     @MessageMapping("/log-fail")
     @SendTo("/api/topic/log-fail")
     EnteringLog enteringLog(EnteringLog enteringLog) {
+        log.info("StompController::enteringLog");
+        log.info("enteringLog: {}", enteringLog);
         return enteringLog;
     }
 

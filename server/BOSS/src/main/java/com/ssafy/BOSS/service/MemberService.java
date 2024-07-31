ab306660906b6f8c183433ac3c7135354dd8fd0a
서정운 <sju9417@gmail.com>
2024-07-31 11:49:08 +0900

    feat: 멤버 전체 기능 조회 API 추가

--- server/BOSS/src/main/java/com/ssafy/BOSS/service/MemberService.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/service/MemberService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -29,5 +30,9 @@ public class MemberService {
 
     public Optional<Member> findbyNfc(String nfc) {
         return memberRepository.findByNfc(nfc);
+    }
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
     }
 }

287108c0f7ad2302a973cea9f75913d9e3c0077e
wonbin <jbs990609@naver.com>
2024-08-01 17:47:45 +0900

    feat: 멤버 필터링 구현중

--- server/BOSS/src/main/java/com/ssafy/BOSS/repository/MemberRepository.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/repository/MemberRepository.java
@@ -2,10 +2,14 @@ package com.ssafy.BOSS.repository;
 
 import com.ssafy.BOSS.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByNfc(String nfc);
+
+    @Query("SELECT new com.ssafy.BOSS.dto.memberDto.MemberLogDto(m.name, m.department.departmentName, m.position.positionName, m.nfc, m.issueCount)" +
+            "FROM Member m" +)
 }

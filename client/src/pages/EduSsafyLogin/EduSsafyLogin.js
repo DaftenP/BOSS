ea44238ac7c04e06a2a1ab3a3dab541e1abc9794
unknown <ever618@naver.com>
2024-08-03 22:05:32 +0900

    design: 에듀싸피 로그인 페이지 중앙 기준으로 확대 축소 되도록 변경

--- client/src/pages/EduSsafyLogin/EduSsafyLogin.js
+++ client/src/pages/EduSsafyLogin/EduSsafyLogin.js
@@ -22,26 +22,28 @@ export default function Main() {
         </span>
       </div>
       {/* <div className={classes['intro-visual-bg']} /> */}
-      <div className={classes['rectangle-2']}>
-        <div className={classes['logo']} />
-        <span className={classes['id']}>아이디</span>
-        <div className={classes['rectangle-3']} />
-        <span className={classes['password']}>비밀번호</span>
-        <div className={classes['rectangle-4']} />
+      <div className={classes['center']}>
+        <div className={classes['rectangle-2']}>
+          <div className={classes['logo']} />
+          <span className={classes['id']}>아이디</span>
+          <div className={classes['rectangle-3']} />
+          <span className={classes['password']}>비밀번호</span>
+          <div className={classes['rectangle-4']} />
 
-        <button className={classes['rectangle-5']} onClick={handleLoginClick}>
-          <span className={classes['login']}>로그인</span>
-        </button>
-        <span className={classes['password-recovery']}>비밀번호 찾기</span>
-        <div className={classes['rectangle-6']} />
-        <span className={classes['id-save']}>아이디 저장</span>
+          <button className={classes['rectangle-5']} onClick={handleLoginClick}>
+            <span className={classes['login']}>로그인</span>
+          </button>
+          <span className={classes['password-recovery']}>비밀번호 찾기</span>
+          <div className={classes['rectangle-6']} />
+          <span className={classes['id-save']}>아이디 저장</span>
+        </div>
+        <span className={classes['samsung-sw-academy']}>
+          SAMSUNG SW ACADEMY
+          <br />
+          FOR YOUTH
+        </span>
+        <span className={classes['ssafy-welcome']}>SSAFY에 오신것을 환영합니다.</span>
       </div>
-      <span className={classes['samsung-sw-academy']}>
-        SAMSUNG SW ACADEMY
-        <br />
-        FOR YOUTH
-      </span>
-      <span className={classes['ssafy-welcome']}>SSAFY에 오신것을 환영합니다.</span>
     </div>
   );
 }

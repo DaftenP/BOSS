0861d2d57b61a74389a94a3e3bb46ab803283f6f
서정운 <sju9417@gmail.com>
2024-08-05 17:18:31 +0900

    fix: 시큐리티 필터 해제
    - 로그인 이외 API가 필터링되는 부분을 임시로 해제

--- server/BOSS/src/main/java/com/ssafy/BOSS/config/SecurityConfig.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/config/SecurityConfig.java
@@ -27,18 +27,16 @@ public class SecurityConfig {
         return httpSecurity
                 // REST API이므로 basic auth 및 csrf 보안을 사용하지 않음
                 .httpBasic(httpBasic -> httpBasic.disable())
-                .cors(cors -> cors.disable())
-                .csrf(csrf -> csrf.disable())
                 // JWT를 사용하기 때문에 세션을 사용하지 않음
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                // 해당 API에 대해서는 모든 요청을 허가
-                                .requestMatchers("/api/admin/sign-in").permitAll()
-                                // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
-                                //.anyRequest().authenticated()
-                )
+//                .authorizeHttpRequests(authorizeRequests ->
+//                        authorizeRequests
+//                                // 해당 API에 대해서는 모든 요청을 허가
+//                                .requestMatchers("/api/admin/sign-in").permitAll()
+//                                // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
+//                                //.anyRequest().authenticated()
+//                )
                 // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class).build();
 

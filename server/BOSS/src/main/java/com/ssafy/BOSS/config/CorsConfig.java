370c601321370eed006ee6e039cec5a793f06d34
slowcloud <sju9417@gmail.com>
2024-08-05 22:10:57 +0900

    feat: CORS 추가 설정
    - 스프링 프레임워크 자체의 CORS 설정

--- server/BOSS/src/main/java/com/ssafy/BOSS/config/CorsConfig.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/config/CorsConfig.java
@@ -7,10 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Bean
     @Order(1)
@@ -19,4 +21,13 @@ public class CorsConfig {
         return http.build();
     }
 
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**") // 허용할 URL
+                .allowedOrigins("*") // 허용할 출처(요청 서버)
+                .allowedMethods("*") // 허용할 HTTP 메소드
+                .allowedHeaders("*"); // 허용할 헤더
+    }
 }

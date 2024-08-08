112a2cab2b25f0c10f7c9208b5829f2bdaa4d54f
slowcloud <sju9417@gmail.com>
2024-08-08 22:23:46 +0900

    feat: PositionMapper 작성

--- server/BOSS/src/main/java/com/ssafy/BOSS/mapper/DepartmentMapper.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/mapper/DepartmentMapper.java
@@ -3,7 +3,6 @@ package com.ssafy.BOSS.mapper;
 import com.ssafy.BOSS.domain.Department;
 import com.ssafy.BOSS.dto.department.DepartmentDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface DepartmentMapper {

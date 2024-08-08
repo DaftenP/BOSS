cf3bdb9b91dc6ed2a3771aec788131c68e4c8f30
서정운 <sju9417@gmail.com>
2024-08-08 10:48:28 +0900

    remove: 남은 캐싱 기능 제거

--- server/BOSS/src/main/java/com/ssafy/BOSS/service/DepartmentService.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/service/DepartmentService.java
@@ -3,8 +3,6 @@ package com.ssafy.BOSS.service;
 import com.ssafy.BOSS.domain.Department;
 import com.ssafy.BOSS.repository.DepartmentRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,12 +13,10 @@ public class DepartmentService {
 
     private final DepartmentRepository departmentRepository;
 
-    @CachePut("departments")
     public void save(Department department) {
         departmentRepository.save(department);
     }
 
-    @Cacheable("departments")
     public List<Department> getAllDepartments() {
         return departmentRepository.findAll();
     }

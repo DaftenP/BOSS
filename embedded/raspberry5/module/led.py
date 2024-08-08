fc060f5b95e8569d25645359194fe63ef6754f96
강현성 <tjd4025@naver.com>
2024-08-08 17:39:19 +0900

    feat: 젯슨과 통신 기능 추가 및 LED 개선

--- embedded/raspberry5/module/led.py
+++ embedded/raspberry5/module/led.py
@@ -1,30 +1,55 @@
 import RPi.GPIO as GPIO
-from time import sleep
+from time import sleep, time
 
 class LED:
-    def __init__(self, pin=16):
+    def __init__(self, green_pin=27, red_pin=22):
         """LED 초기화 및 핀 설정"""
-        self.LED_pin = pin
-        GPIO.setup(self.LED_pin, GPIO.OUT)
+        self.green_pin = green_pin
+        self.red_pin = red_pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.green_pin, GPIO.OUT)
+        GPIO.setup(self.red_pin, GPIO.OUT)
         
-    def turn_on(self):
-        """LED를 켭니다."""
-        GPIO.output(self.LED_pin, True)
+    def turn_on_green(self):
+        """초록색 LED를 켭니다."""
+        GPIO.output(self.green_pin, True)
         
-    def turn_off(self):
-        """LED를 끕니다."""
-        GPIO.output(self.LED_pin, False)
+    def turn_off_green(self):
+        """초록색 LED를 끕니다."""
+        GPIO.output(self.green_pin, False)
+    
+    def turn_on_red(self):
+        """빨간색 LED를 켭니다."""
+        GPIO.output(self.red_pin, True)
+        
+    def turn_off_red(self):
+        """빨간색 LED를 끕니다."""
+        GPIO.output(self.red_pin, False)
 
-    def blink(self, duration=1, interval=0.5):
-        """LED를 깜박이게 합니다.
+    def blink_green(self, duration=5, interval=0.5):
+        """초록색 LED를 깜박이게 합니다.
         duration: 전체 깜박이는 시간 (초)
         interval: 깜박임 간격 (초)
         """
-        self.turn_on()
-        sleep(interval)
-        self.turn_off()
-        sleep(interval)
-        self.turn_on()
-        sleep(interval)
-        self.turn_off()
-        sleep(interval)
+        end_time = time() + duration
+        while time() < end_time:
+            self.turn_on_green()
+            sleep(interval)
+            self.turn_off_green()
+            sleep(interval)
+
+    def blink_red(self, duration=5, interval=0.5):
+        """빨간색 LED를 깜박이게 합니다.
+        duration: 전체 깜박이는 시간 (초)
+        interval: 깜박임 간격 (초)
+        """
+        end_time = time() + duration
+        while time() < end_time:
+            self.turn_on_red()
+            sleep(interval)
+            self.turn_off_red()
+            sleep(interval)
+            
+    def cleanup():
+        """GPIO 설정 정리"""
+        GPIO.cleanup()

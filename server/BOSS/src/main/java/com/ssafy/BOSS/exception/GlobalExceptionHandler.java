8d7bfc13cf0e44e8594863273fbc77c5d18d5d37
서정운 <sju9417@gmail.com>
2024-08-12 14:52:27 +0900

    remove: 불필요해진 코드 제거

--- server/BOSS/src/main/java/com/ssafy/BOSS/exception/GlobalExceptionHandler.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.ssafy.BOSS.exception;
 import com.ssafy.BOSS.exception.errorCode.ErrorCode;
 import com.ssafy.BOSS.exception.errorCode.ErrorCodeProblemDetail;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,12 +17,6 @@ public class GlobalExceptionHandler {
         ErrorCode errorCode = e.getErrorCode();
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(errorCode.getHttpStatus(), errorCode.getMessage());
         return new ErrorCodeProblemDetail(problemDetail, errorCode.getErrorCode());
-    }
-
-    @ExceptionHandler
-    public ProblemDetail handleException(Exception e) {
-        log.error(e.getMessage());
-        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
 }

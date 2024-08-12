f5f496da81cbfa2e45e538e7711d01c8c89cbcba
서정운 <sju9417@gmail.com>
2024-08-12 15:18:09 +0900

    feat: 컴파일 시, 할당되지 않은 변수에 대한 경고문이 뜨지 않도록 수정

--- server/BOSS/src/main/java/com/ssafy/BOSS/mapper/EnteringLogMapper.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/mapper/EnteringLogMapper.java
@@ -5,8 +5,9 @@ import com.ssafy.BOSS.domain.Member;
 import com.ssafy.BOSS.dto.enteringLog.EnteringLogDto;
 import com.ssafy.BOSS.dto.enteringLog.EnteringLogRegistDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EnteringLogMapper {
     EnteringLogDto enteringLogToEnteringLogDto(EnteringLog enteringLog);
 

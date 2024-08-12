cdbcc74c78eece820534154f5d024c05b577079b
서정운 <sju9417@gmail.com>
2024-08-12 15:35:21 +0900

    refactor: 매핑 코드를 매퍼로 이동

--- server/BOSS/src/main/java/com/ssafy/BOSS/mapper/MemberMapper.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/mapper/MemberMapper.java
@@ -1,13 +1,76 @@
 package com.ssafy.BOSS.mapper;
 
+import com.ssafy.BOSS.domain.Department;
 import com.ssafy.BOSS.domain.Member;
+import com.ssafy.BOSS.domain.Position;
 import com.ssafy.BOSS.dto.memberDto.MemberDto;
 import com.ssafy.BOSS.dto.memberDto.MemberLoginDto;
-import org.mapstruct.Mapper;
+import com.ssafy.BOSS.dto.memberDto.MemberRegistDto;
+import com.ssafy.BOSS.repository.DepartmentRepository;
+import com.ssafy.BOSS.repository.PositionRepository;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
-public interface MemberMapper {
-    MemberDto memberToMemberDto(Member member);
+public abstract class MemberMapper {
 
-    MemberLoginDto memberToMemberLoginDto(Member member);
+    private DepartmentRepository departmentRepository;
+    private PositionRepository positionRepository;
+
+    @BeforeMapping
+    public void injectRepository(DepartmentRepository departmentRepository, PositionRepository positionRepository) {
+        this.departmentRepository = departmentRepository;
+        this.positionRepository = positionRepository;
+    }
+
+    abstract public MemberDto memberToMemberDto(Member member);
+
+    abstract public MemberLoginDto memberToMemberLoginDto(Member member);
+
+    @Mapping(target = "profileImage", constant = "")
+    public Member memberRegistDtoToMember(MemberRegistDto memberRegistDto) {
+        if(memberRegistDto == null) {
+            return null;
+        }
+
+        Member member = new Member();
+
+        member.setName( memberRegistDto.getName() );
+        member.setMemberLoginId( memberRegistDto.getMemberLoginId() );
+        member.setMemberLoginPw( memberRegistDto.getMemberLoginPw() );
+        member.setNfc( memberRegistDto.getNfc() );
+        member.setPhoneNumber( memberRegistDto.getPhoneNumber() );
+
+        member.setProfileImage( "" );
+
+        member.setDepartment(getOrCreateDepartment(memberRegistDto));
+        member.setPosition(getOrCreatePosition(memberRegistDto));
+
+        return member;
+    }
+
+
+    private Department getOrCreateDepartment(MemberRegistDto memberRegistDto) {
+        if (memberRegistDto.getDepartmentId() != -1) {
+            return departmentRepository.getReferenceById(memberRegistDto.getDepartmentId());
+        }
+        if (departmentRepository.existsByDepartmentName(memberRegistDto.getDepartmentName())) {
+            return departmentRepository.findByDepartmentName(memberRegistDto.getDepartmentName());
+        }
+        Department department = Department.builder().departmentName(memberRegistDto.getDepartmentName()).build();
+        departmentRepository.save(department);
+        return department;
+    }
+
+    private Position getOrCreatePosition(MemberRegistDto memberRegistDto) {
+        if (memberRegistDto.getPositionId() != -1) {
+            return positionRepository.getReferenceById(memberRegistDto.getPositionId());
+        }
+        if (positionRepository.existsByPositionName(memberRegistDto.getPositionName())) {
+            return positionRepository.findByPositionName(memberRegistDto.getPositionName());
+        }
+        Position position = Position.builder().positionName(memberRegistDto.getPositionName()).build();
+        positionRepository.save(position);
+        return position;
+    }
 }

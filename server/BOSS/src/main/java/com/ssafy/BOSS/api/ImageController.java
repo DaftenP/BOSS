56bc0f1410eddc389c524c534da994d172cac7bd
wonbin <jbs990609@naver.com>
2024-08-13 11:53:34 +0900

    feat: 이미지 사이즈 줄여서 저장 구현

--- server/BOSS/src/main/java/com/ssafy/BOSS/api/ImageController.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/api/ImageController.java
@@ -3,10 +3,7 @@ package com.ssafy.BOSS.api;
 import com.ssafy.BOSS.service.S3UploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
@@ -16,9 +13,9 @@ public class ImageController {
 
     private final S3UploadService s3UploadService;
 
-    @PostMapping("/upload")
-    public ResponseEntity<?> s3Upload(@RequestPart(value = "image", required = false) MultipartFile image) {
-        String profileImage = s3UploadService.upload(image);
+    @PostMapping("/upload/{var}")
+    public ResponseEntity<?> s3Upload(@RequestPart(value = "image", required = false) MultipartFile image, @PathVariable int var) {
+        String profileImage = s3UploadService.upload(image, var);
         return ResponseEntity.ok(profileImage);
     }
 }
--- server/BOSS/src/main/java/com/ssafy/BOSS/service/S3UploadService.java
+++ server/BOSS/src/main/java/com/ssafy/BOSS/service/S3UploadService.java
@@ -7,12 +7,14 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.util.IOUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
@@ -37,6 +39,13 @@ public class S3UploadService {
         return this.uploadImage(image);
     }
 
+    public String upload(MultipartFile image, int var) {
+        //입력받은 이미지 파일이 빈 파일인지 검증
+        validateEmptyImage(image);
+        //uploadImage를 호출하여 S3에 저장된 이미지의 public url을 반환한다.
+        return this.uploadImage(image, var);
+    }
+
     private void validateEmptyImage(MultipartFile image) {
         if (image.isEmpty() || Objects.isNull(image.getOriginalFilename())) {
             throw new MultipartException("파일이 비었음. " + image.getOriginalFilename());
@@ -47,6 +56,15 @@ public class S3UploadService {
         validateImageFileExtension(image.getOriginalFilename());
         try {
             return this.uploadImageToS3(image);
+        } catch (IOException e) {
+            throw new MultipartException(image.getOriginalFilename());
+        }
+    }
+
+    private String uploadImage(MultipartFile image, int var) {
+        validateImageFileExtension(image.getOriginalFilename());
+        try {
+            return this.uploadImageToS3(image, var);
         } catch (IOException e) {
             throw new MultipartException(image.getOriginalFilename());
         }
@@ -87,6 +105,54 @@ public class S3UploadService {
             //생성자 : bucket 이름, 파일 명, byteInputStream, metadata
             PutObjectRequest putObjectRequest =
                     new PutObjectRequest(bucketName, s3FileName, byteArrayInputStream, metadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead);
+
+            //실제로 S3에 이미지 데이터를 넣는 부분이다.
+            amazonS3.putObject(putObjectRequest); // put image to S3
+        } catch (Exception e) {
+            System.out.println(amazonS3.getUrl(bucketName, s3FileName).toString());
+            e.printStackTrace();
+            throw new MultipartException("S3 넣기 실패!: " + originalFilename);
+        } finally {
+            byteArrayInputStream.close();
+            is.close();
+        }
+
+        String url = amazonS3.getUrl(bucketName, s3FileName).toString();
+        String cdn = "https://d3vud5llnd72x5.cloudfront.net/" + url.split("/")[url.split("/").length - 1];
+
+        return cdn;
+    }
+
+    private String uploadImageToS3(MultipartFile image, int var) throws IOException {
+        String originalFilename = image.getOriginalFilename(); //원본 파일 명
+        String extension = originalFilename.substring(originalFilename.lastIndexOf(".")); //확장자 명
+
+        String s3FileName = UUID.randomUUID().toString().substring(0, 10) + originalFilename; //변경된 파일 명
+
+        InputStream is = image.getInputStream();
+        byte[] bytes = IOUtils.toByteArray(is); //image를 byte[]로 변환
+
+        //S3에 요청할 때 사용할 byteInputStream 생성
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        Thumbnails.of(byteArrayInputStream)
+                .size(200, 300)
+                .keepAspectRatio(false)
+                .toOutputStream(os);
+
+        byte[] compressedImage = os.toByteArray();
+        ByteArrayInputStream uploadInputStream = new ByteArrayInputStream(compressedImage);
+
+        ObjectMetadata metadata = new ObjectMetadata(); //metadata 생성
+        metadata.setContentType("image/" + extension);
+        metadata.setContentLength(compressedImage.length);
+
+        try {
+            //S3로 putObject 할 때 사용할 요청 객체
+            //생성자 : bucket 이름, 파일 명, byteInputStream, metadata
+            PutObjectRequest putObjectRequest =
+                    new PutObjectRequest(bucketName, s3FileName, uploadInputStream, metadata)
                             .withCannedAcl(CannedAccessControlList.PublicRead);
 
             //실제로 S3에 이미지 데이터를 넣는 부분이다.
